9a4462d846066cbc32e854dba6eef9f0a0a50613
nutrii182 <nutrii182@gmail.com>
2020-10-29 13:17:44 -0600

    Picker y Uploader de Curso

--- ProyectoCore/cursoapp/src/components/Cursos/NuevoCurso.js
+++ ProyectoCore/cursoapp/src/components/Cursos/NuevoCurso.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Button,
   Container,
@@ -6,9 +6,45 @@ import {
   TextField,
   Typography,
 } from "@material-ui/core";
+import {
+  KeyboardDatePicker,
+  MuiPickersUtilsProvider,
+} from "@material-ui/pickers";
+import DateFnsUtils from "@date-io/date-fns";
 import style from "../shared/Style";
+import ImageUploader from "react-images-upload";
+import { v4 as uuidv4 } from "uuid";
+import { getDataImage  } from "../../actions/ImagenAction";
 
 const NuevoCurso = () => {
+  const [fechaSelect, setFechaSelect] = useState(new Date());
+  const [imagenCurso, setImagenCurso] = useState(null);
+  const [curso, setCurso] = useState({
+    titulo: "",
+    descripcion: "",
+    precio: 0.0,
+    promocion: 0.0,
+  });
+
+  const ingresarValores = (e) => {
+    const { name, value } = e.target;
+
+    setCurso((ant) => ({
+      ...ant,
+      [name]: value,
+    }));
+  };
+
+  const subirFoto = imagenes => {
+      const foto = imagenes[0];
+
+      getDataImage(foto).then((resp) => {
+          setImagenCurso(resp);
+      });
+  };
+
+  const fotoKey = uuidv4();
+
   return (
     <Container component="main" maxWidth="md" justify="center">
       <div style={style.paper}>
@@ -19,34 +55,69 @@ const NuevoCurso = () => {
           <Grid container spacing={2}>
             <Grid item xs={12} md={12}>
               <TextField
-                name="Titulo"
+                name="titulo"
                 variant="outlined"
                 fullWidth
                 label="Ingrese Título"
+                value={curso.titulo}
+                onChange={ingresarValores}
               />
             </Grid>
             <Grid item xs={12} md={12}>
               <TextField
-                name="Descripción"
+                name="descripcion"
                 variant="outlined"
                 fullWidth
                 label="Ingrese Descripción"
+                value={curso.descripcion}
+                onChange={ingresarValores}
               />
             </Grid>
             <Grid item xs={12} md={6}>
               <TextField
-                name="Precio"
+                name="precio"
                 variant="outlined"
                 fullWidth
                 label="Precio del Curso"
+                value={curso.precio}
+                onChange={ingresarValores}
               />
             </Grid>
             <Grid item xs={12} md={6}>
               <TextField
-                name="Promocion"
+                name="promocion"
                 variant="outlined"
                 fullWidth
                 label="Precio de Promoción"
+                value={curso.promocion}
+                onChange={ingresarValores}
+              />
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                <KeyboardDatePicker
+                  value={fechaSelect}
+                  onChange={setFechaSelect}
+                  margin="normal"
+                  id="fecha-publicion-Id"
+                  label="Seleccione Fecha de Publicación"
+                  format="dd/MM/yyyy"
+                  fullWidth
+                  KeyboardButtonProps={{
+                    "aria-label": "change date",
+                  }}
+                />
+              </MuiPickersUtilsProvider>
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <ImageUploader
+                withIcon={false}
+                key={fotoKey}
+                singleImage={true}
+                buttonText="Imagen del Curso"
+                onChange={subirFoto}
+                imgExtension={[".jpg", ".gif", ".png", ".jpeg"]}
+                maxFileSize = {5242880}
               />
             </Grid>
           </Grid>
